6c04597bfe1c75edec6821154885c5f5840b1d86
romaniv2511 <romaniv2511@gmail.com>
2023-01-03 00:56:52 +0200

    add auth operations

---
+++ src/redux/auth/authOperations.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `${token}`;
+  },
+  unset(token) {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+export const signup = createAsyncThunk(
+  'auth/signup',
+  async (user, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/signup', user);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (user, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/login', user);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+export const logout = createAsyncThunk(
+  'auth/logout',
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/logout');
+      token.unset();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
