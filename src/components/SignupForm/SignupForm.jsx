90fa2783ed45e9566c6d6dced85492bc0cb491ac
romaniv2511 <romaniv2511@gmail.com>
2023-01-03 20:38:14 +0200

    create SIgnupForm

---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,33 @@
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+
+export const SignupForm = () => {
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+    // dispatch()
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        <input type="text" name="name" />
+      </label>
+      <label>
+        <input type="email" name="email" />
+      </label>
+      <label>
+        <input type="text" name="password" />
+      </label>
+
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
